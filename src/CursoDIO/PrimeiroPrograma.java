7eec4ea496f7bfc1dce0a38f3cf813888808c591
Joao Samuel <joaosamuel.2001@gmail.com>
2021-11-26 14:45:00 -0300

    First Commit

---
+++ src/CursoDIO/PrimeiroPrograma.java
@@ -0,0 +1,14 @@
+package CursoDIO;
+
+import CursoDIO.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        /*int a = 2;
+        int b = 3;
+        System.out.println("Hello World! " + (a+b));*/
+
+        Gato gato = new Gato();
+        System.out.println(gato);
+    }
+}
